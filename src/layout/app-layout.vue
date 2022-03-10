72dc6a2a0c3d8a85624195b7a6b1d58660b67701
flinzhu <zhufl@gravitylink.com>
2022-03-10 17:05:41 +0800

    feat: 页面布局配置

---
+++ src/layout/app-layout.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      Aside
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style lang="scss" scoped>
+ .el-header {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+}
+
+ .el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+
+}
+
+ .el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+}
+
+.el-container{
+  height: 100vh;
+}
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,25 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
+import AppLayout from '@/layout/app-layout.vue'
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'home',
-    component: () => import('../views/home/home-index.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/login/login-index.vue')
+    component: AppLayout,
+    children: [
+      {
+        path: '', // '' 则为默认子路由
+        name: 'home',
+        component: () => import('../views/home/home-index.vue')
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/login/login-index.vue')
+      }
+    ]
   }
+
 ]
 
 const router = createRouter({
