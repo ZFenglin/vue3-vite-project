efbed77355b032f4bae7c749479346da32b8e1c0
flinzhu <zhufl@gravitylink.com>
2022-03-11 10:24:28 +0800

    feat: 页面加载进度条添加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-
 import AppLayout from '@/layout/app-layout.vue'
-
 import permissionRoutes from './modules/permission'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -32,5 +32,13 @@ const router = createRouter({
   history: createWebHashHistory(), // 路由模式
   routes: routes
 })
+// 页面进度条添加
+// npm i --save nprogress
+router.beforeEach(() => {
+  nprogress.start()
+})
+router.afterEach(() => [
+  nprogress.done()
+])
 
 export default router
